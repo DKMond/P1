7586f90ddf14cf96a421a0031044c6d085217786
DKMond <madem.ionut0@gmail.com>
2020-10-11 20:20:23 +0300

    ceva

--- Temă.py
+++ Temă.py
@@ -1,18 +1,30 @@
-# print("Exercitiul 1")
-# v1 = [1,2,3,4,5,6,7,8,9,10]
-# for EachNumber in v1:
-#     print(EachNumber)
-# __________________________________________________________________
-# print("Exercitiul 2")
-# V1 = 12345678 #(Parola reala a contului)
-# EMAIL_ADDRESS = "george2020@ggfox.com"
-# EMAIL_PASSWORD = 12345677
-# if EMAIL_PASSWORD == V1: print("Emailul este valid")
-# elif EMAIL_PASSWORD < V1: print("Emailul este invalid")
-# __________________________________________________________________
+print("Exercitiul 1")
+V1 = [1,2,3,4,5,6,7,8,9,10]
+for EachNumber in V1:
+    print(EachNumber)
+#__________________________________________________________________
+print("Exercitiul 2")
+
+EMAIL_ADDRESS = "george2020@ggfo.com"
+EMAIL_PASSWORD = "12345678"
+
+
+V2 = "12345678"
+E1 = "george2020@ggfo.com"
+if len(EMAIL_PASSWORD) == len(V2) and EMAIL_PASSWORD == V2:
+    EMAIL_PASSWORD = True
+if len(EMAIL_ADDRESS) == len(E1)\
+        and EMAIL_ADDRESS == E1:
+     EMAIL_ADDRESS = True
+
+if EMAIL_ADDRESS and EMAIL_PASSWORD is True:
+    print("Permisiune acordata")
+else:
+    print("Emailul sau parola sunt incorecte")
+#__________________________________________________________________
 print("Exercitiul 3")
-v1 = False
-if v1 is True: v1 = 3
-elif v1 is False: v1 = 9
-if v1 == 3: print("poof")
-elif v1 == 9: print("presto")
+V3 = "0767421824"
+if len(V3) == 10:print("numarul este valid")
+elif len(V3) < 10:print("numarul este invalid")
+elif len(V3) > 10:print("numarul este invalid:")
+#__________________________________________________________________
