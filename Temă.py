d9d9c4dcea8af55489a67e2888b3e3c0024b19e0
DKMond <madem.ionut0@gmail.com>
2020-10-11 17:41:19 +0300

    ceva

---
+++ Temă.py
@@ -0,0 +1,10 @@
+# print("Exercitiul 1")
+# v1 = [1,2,3,4,5,6,7,8,9,10]
+# for EachNumber in v1:
+#     print(EachNumber)
+print("Exercitiul 2")
+V1 = 12345678
+EMAIL_ADDRESS = "george2020@ggfox.com"
+EMAIL_PASSWORD = 12345677
+if EMAIL_PASSWORD == V1: print("Emailul este valid")
+elif EMAIL_PASSWORD < V1: print("Emailul este invalid")
