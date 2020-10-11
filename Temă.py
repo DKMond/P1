320ed08bca479dac6e76cdbe2e666827e17307a7
DKMond <madem.ionut0@gmail.com>
2020-10-11 18:18:28 +0300

    mesaj

--- Temă.py
+++ Temă.py
@@ -2,9 +2,17 @@
 # v1 = [1,2,3,4,5,6,7,8,9,10]
 # for EachNumber in v1:
 #     print(EachNumber)
+# __________________________________________________________________
 # print("Exercitiul 2")
-# V1 = 12345678
+# V1 = 12345678 #(Parola reala a contului)
 # EMAIL_ADDRESS = "george2020@ggfox.com"
 # EMAIL_PASSWORD = 12345677
 # if EMAIL_PASSWORD == V1: print("Emailul este valid")
 # elif EMAIL_PASSWORD < V1: print("Emailul este invalid")
+# __________________________________________________________________
+print("Exercitiul 3")
+v1 = False
+if v1 is True: v1 = 3
+elif v1 is False: v1 = 9
+if v1 == 3: print("poof")
+elif v1 == 9: print("presto")
