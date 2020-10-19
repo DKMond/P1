ea4c1f1f75a4075221a06cf7a7cfcb4ff420ab71
DKMond <madem.ionut0@gmail.com>
2020-10-19 18:42:40 +0300

    tema neterminata

--- Temă.py
+++ Temă.py
@@ -1,31 +1,40 @@
 print("Exercitiul 1")
-V1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for EachNumber in V1:
-    print(EachNumber)
-# __________________________________________________________________
+
+for V1 in range(11):
+    print(V1)
+# ____________________________________________________________________________
 print("Exercitiul 2")
 
-EMAIL_ADDRESS = "george2020@ggfo.com"
-EMAIL_PASSWORD = "12345678"
+print('Te rugam introduce emailul si parola pentru a continua')
+EMAIL_ADRESS = input('Adresa de email: ')
+EMAIL_PASSWORD = input('Parola: ')
 
-V2 = "12345678"
-E1 = "george2020@ggfo.com"
-if len(EMAIL_PASSWORD) == len(V2) and EMAIL_PASSWORD == V2:
-    EMAIL_PASSWORD = True
-if len(EMAIL_ADDRESS) == len(E1) \
-        and EMAIL_ADDRESS == E1:
-    EMAIL_ADDRESS = True
-if EMAIL_ADDRESS and EMAIL_PASSWORD is True:
-    print("Permisiune acordata")
+if EMAIL_ADRESS == 'george21@foxgg.com' and EMAIL_PASSWORD == 'Ceva2123':
+    print("Bun Venit George")
 else:
-    print("Emailul sau parola sunt incorecte")
-# __________________________________________________________________
+    print('Emailul sau parola sunt incorecte')
+# _____________________________________________________________________________
 print("Exercitiul 3")
-V3 = "0767421824"
+V3 = input()
 if len(V3) == 10:
     print("numarul este valid")
 elif len(V3) < 10:
     print("numarul este invalid")
 elif len(V3) > 10:
     print("numarul este invalid:")
-# __________________________________________________________________
+# _______________________________________________________________________________
+print("Exercitiul 4")
+print("Verificator de cnp-uri")
+CNP = input()
+
+if len(CNP) == 13:
+    CNP = True
+elif len(CNP) < 13:
+    CNP = False
+elif len(CNP) > 13:
+    CNP = False
+if CNP is True:
+    print('CNP-ul este valid')
+elif CNP is False:
+    print('CNP-ul este invalid')
+# Nu mai am ideii
